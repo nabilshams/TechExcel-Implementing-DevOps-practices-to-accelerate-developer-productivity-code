b0d0318b9917c7d5400bf54554582c095ac3b842
Nabil Siddiqui <nabil_shams@hotmail.com>
2023-12-15 14:53:17 +1100

    Update Message.cs

--- Application/src/RazorPagesTestSample/Data/Message.cs
+++ Application/src/RazorPagesTestSample/Data/Message.cs
@@ -7,6 +7,7 @@ namespace RazorPagesTestSample.Data
     {
         // testing the CI workflow 3rd time
         // building container image in the workflow.
+        // added needs for deploy
         public int Id { get; set; }
 
         [Required]
